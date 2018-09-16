4c4107e9c141faeb0ba66d79f15c9cf14daaef42
GermainZ <germanosz@gmail.com>
2018-09-16 14:31:30 +0200

    Add short delay before creating identicon for new contacts (resolves #13)

--- identiconizer/src/main/java/com/germainz/identiconizer/services/ContactsObserverService.java
+++ identiconizer/src/main/java/com/germainz/identiconizer/services/ContactsObserverService.java
@@ -21,6 +21,7 @@ import android.content.Intent;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.database.CursorIndexOutOfBoundsException;
+import android.os.Handler;
 import android.os.IBinder;
 import android.provider.ContactsContract;
 
@@ -88,25 +89,37 @@ public class ContactsObserverService extends Service {
         @Override
         public void onChange(boolean selfChange) {
             super.onChange(selfChange);
-            // ContentObserver doesn't tell us anything about the nature of the change (addition,
-            // deletion, editing, etc.)
-            // To see if a new contact has been added, we compare the current highest contact ID
-            // with the previously stored one.
-            Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
-                    new String[]{ContactsContract.Contacts._ID},
-                    ContactsContract.Contacts.IN_VISIBLE_GROUP,
-                    null, "_id DESC LIMIT 1");
-            cursor.moveToFirst();
-            int maxContactID = cursor.getInt(cursor.getColumnIndex("_id"));
-            cursor.close();
-            if (maxContactID == mMaxContactID)
-                return;
-            if (maxContactID > mMaxContactID)
-                startService(new Intent(getApplicationContext(),
-                        IdenticonCreationService.class).putExtra("updateExisting", false));
-            mMaxContactID = maxContactID;
-            mConfig.setMaxContactID(mMaxContactID);
+            // Small delay to give DAVdroid (and possibly other programs) enough time to set a
+            // contact's initial photo, if any.
+            // See <https://github.com/GermainZ/Identiconizer/issues/13>
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    checkChanges();
+                }
+            }, 5000);
         }
+    }
+
+    private void checkChanges() {
+        // ContentObserver doesn't tell us anything about the nature of the change (addition,
+        // deletion, editing, etc.)
+        // To see if a new contact has been added, we compare the current highest contact ID
+        // with the previously stored one.
+        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                new String[]{ContactsContract.Contacts._ID},
+                ContactsContract.Contacts.IN_VISIBLE_GROUP,
+                null, "_id DESC LIMIT 1");
+        cursor.moveToFirst();
+        int maxContactID = cursor.getInt(cursor.getColumnIndex("_id"));
+        cursor.close();
+        if (maxContactID == mMaxContactID)
+            return;
+        if (maxContactID > mMaxContactID)
+            startService(new Intent(getApplicationContext(),
+                    IdenticonCreationService.class).putExtra("updateExisting", false));
+        mMaxContactID = maxContactID;
+        mConfig.setMaxContactID(mMaxContactID);
     }
 
 }
